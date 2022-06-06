b54ba3eb6a7ff512734c411971ff02072f101456
soohwan.ok <soohwan.ok@charlezz.com>
2022-06-06 10:36:14 +0900

    opencv 그레이들 kts로 마이그레이션

---
+++ opencv/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    compileSdk = Dependencies.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = Dependencies.MIN_SDK
+        targetSdk = Dependencies.TARGET_SDK
+
+        externalNativeBuild {
+            cmake {
+                arguments.add("-DANDROID_STL=c++_shared")
+                targets.add("opencv_jni_shared")
+            }
+        }
+    }
+
+    buildTypes {
+        debug {
+            packagingOptions {
+                jniLibs.keepDebugSymbols.add("**/*.so") // controlled by OpenCV CMake scripts
+            }
+        }
+        release {
+            packagingOptions {
+                jniLibs.keepDebugSymbols.add("**/*.so") // controlled by OpenCV CMake scripts
+            }
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.txt")
+        }
+    }
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_11)
+        targetCompatibility(JavaVersion.VERSION_11)
+    }
+
+    sourceSets["main"].jniLibs.srcDirs("native/libs")
+    sourceSets["main"].java.srcDirs("java/src")
+    sourceSets["main"].aidl.srcDirs("java/src")
+    sourceSets["main"].res.srcDirs("java/res")
+    sourceSets["main"].manifest.srcFile("java/AndroidManifest.xml")
+
+    externalNativeBuild {
+        cmake {
+            path = file("${project.projectDir.toString()}/libcxx_helper/CMakeLists.txt")
+        }
+    }
+}
+
+dependencies {
+}
